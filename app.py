fcbbc3eb23b73e913ccea3c656358e8b65b2bd7e
Markuss Palācis <77782410+markieris@users.noreply.github.com>
2026-04-13 15:19:08 +0300

    v0.2

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request, redirect
+import sqlite3
+
+app = Flask(__name__)
+
+#Datubāze
+def make_db():
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT
+            password TEXT
+        )
+        
+    """)
+    conn.commit()
+    conn.close()
+
+make_db()
+
+#Start Page
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+#Register
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("users.db")
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO users (usrname, password) VALUES (?, ?)", (username, password))
+        conn.commit()
+        conn.close()
+
+        return redirect("/login")
+    
+    return render_template("register.html")
+
+#Login
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("users.db")
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+        user = cursor.fetchcone()
+        conn.close()
+
+        if user:
+            return "You Have Logged In!"
+        else:
+            return "Wrong Username or Password!"
+        
+    return render_template("login.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
